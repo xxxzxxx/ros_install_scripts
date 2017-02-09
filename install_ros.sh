aeddef4f22e0974ffca7c941db8115a8d2c26bfd
yusuke kamei <y-kamei@seaos.co.jp>
2017-02-09 18:18:35 +0900

    Fix installation check was buggy. Fix make .ros_profile. Fix load .ros_profile.

--- install_ros.sh
+++ install_ros.sh
@@ -2,27 +2,25 @@
 . ./env.sh
 is_installed () {
     local package=$1
-    return $(dpkg -l | grep ${package} | wc -l)
+    return "$(dpkg -l | grep ${package} | wc -l)"
 }
 echo "Installing dependencies..."
 
-ROS_INSTALLED=$(is_installed "ros-${ROS_CODENAME}")
-if [ "0"=="${ROS_INSTALLED}" ] ; then
+$(is_installed "ros-${ROS_CODENAME}")
+ROS_INSTALLED=$?
+echo "ROS_INSTALLED:${ROS_INSTALLED}"
+
+if [ "0" = ${ROS_INSTALLED} ] ; then
     echo ">Installing ROS Indigo. "
     echo "deb http://packages.ros.org/ros/ubuntu ${OS_CODENAME} main" > /tmp/ros-latest.list
     sudo mv -f /tmp/ros-latest.list /etc/apt/sources.list.d/ros-latest.list
     sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
 fi
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-
-if [ "0"=="$(is_installed 'openssh-server')" ]; then
-    sudo apt install -y openssh-server
-fi
-
-if [ "0"=="${ROS_INSTALLED}" ]; then
+if [ "0" = ${ROS_INSTALLED} ]; then
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+    sudo apt-get -y dist-upgrade
     printf 'What type of ROS installation would you like to use. [desktop-full, desktop, ros-base]:'
     reply=""
     read reply
@@ -34,6 +32,19 @@ if [ "0"=="${ROS_INSTALLED}" ]; then
     target="ros-${ROS_CODENAME}-${reply}"
     echo "installing ${target}, python-rosinstall ..."
     sudo apt install -y ${target} python-rosinstall
-#    sudo rosdep init
-#    rosdep update
+fi
+if [ ! -f ~/.ros_profile ]; then
+printf "export PATH=/opt/ros/${ROS_CODENAME}/bin/;" > ~/.ros_profile
+echo '${PATH}' >> ~/.ros_profile
+echo '
+if [ -f ${HOME}/.ros_profile ]; then
+  source ${HOME}/.ros_profile
+fi
+' >> ${HOME}/.profile
+fi
+
+if [ "0" = ${ROS_INSTALLED} ]; then
+     :
+else
+     echo "ros installed."
 fi
